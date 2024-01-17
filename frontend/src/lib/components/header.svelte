28ae8af34f847f9a03d9188ce7993d2f6f276d60
LackOfVitamins <r.haijtema@gmail.com>
2024-01-17 14:31:56 +0100

    [Frontend] Added header

---
+++ frontend/src/lib/components/header.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+  import ModeToggle from "$components/mode-toggle.svelte";
+  import * as Avatar from "$lib/components/ui/avatar";
+</script>
+
+<header class="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+  <div class="container flex justify-between items-center max-w-screen-2xl h-16 sm:h-20">
+    <div>
+      <a href="/" class="flex items-center space-x-2">
+        <span class="font-bold  sm:text-xl">
+          AICook
+        </span>
+      </a>
+    </div>
+    <div>
+      <nav class="flex items-center">
+        <ModeToggle />
+
+        <Button variant="ghost" size="icon">
+          <Avatar.Root>
+            <Avatar.Image src="" alt="example" />
+            <Avatar.Fallback>EX</Avatar.Fallback>
+          </Avatar.Root>
+        </Button>
+      </nav>
+    </div>
+  </div>
+</header>
