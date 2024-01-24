711909f2596b95874bdbd6179d0c89f8c0dc003b
LackOfVitamins <r.haijtema@gmail.com>
2024-01-24 17:10:17 +0100

    [Backend] Fixed CORS

--- AICook.Gateway/Program.cs
+++ AICook.Gateway/Program.cs
@@ -7,9 +7,22 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddReverseProxy()
             .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("cors", builder =>
+            {
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+                builder.AllowAnyOrigin();
+            });
+        });
         
         var app = builder.Build();
+
+        app.UseCors();
         app.MapReverseProxy();
+
         app.Run();
     }
 }
